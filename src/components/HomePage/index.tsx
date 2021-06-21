9bce84ac45b416173627e0e73c4e9a405cb64d87
Bruno Zutim <bzutim@gmail.com>
2021-06-20 23:33:26 -0300

    day 1

---
+++ src/components/HomePage/index.tsx
@@ -0,0 +1,12 @@
+import React, { FormEvent } from 'react';
+
+import styles from './styles.module.scss';
+
+export default function HomePage() {
+
+    return (
+        <div className={styles.container}>
+            <h1>Vai</h1>
+        </div>
+    );
+}
